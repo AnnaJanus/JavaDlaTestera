adff32d85f60010db8447f9fdb6822bac1e76020
AnnaJanus <anusiula.789@gmail.com>
2024-04-02 22:07:20 +0200

    Person class changed

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,15 @@
 public class Person {
-    int age = 40;
-    String name = "Jan Kowalski";
+    String name;
+    String job;
+    boolean alreadyInIT;
+    int programmingLanguages;
+    String whyThisCourse;
 
-    public void talk(){
-        System.out.println("Hello");
+    public void talk() {
+        System.out.println("Name: ");
+        System.out.println("Job: ");
+        System.out.println("Already In IT: ");
+        System.out.println("Programming Languages: ");
+        System.out.println("Why this course: ");
     }
 }
